a42e0e873cdd540f1cafe8641ce82b39481923a2
Kevin Nathaniel <kevinnathaniel497@yahoo.com>
2020-03-16 19:39:46 +0700

    CRUD for topic

--- db/init.js
+++ db/init.js
@@ -1,119 +1,120 @@
 const mongoose = require('mongoose');
 const db = require('./models');
-const {Topic, Course, Program, Teacher, Admin, Student, EnrollProgram} = db;
 
-async function initialize(){
+const {
+  Topic, Course, Program, Teacher, Admin, Student, EnrollProgram,
+} = db;
 
-    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-        .catch((error) => {
-            console.log("DB error", error);
-        });
-
-    var topic1 = await new Topic({
-        name : 'Inheritance'
-    }).save();
-
-    var topic2 = await new Topic({
-        name : 'Exception'
-    }).save();
-
-    var topic3 = await new Topic({
-        name : 'Android'
-    }).save();
-
-    var course1 = await new Course({
-        name: 'Object-oriented Programming',
-        code : 'IF2110',
-        description : 'lorem ipsum',
-        list_topic : [topic1.id, topic2.id]
-    }).save();
-
-    var course2 = await new Course({
-        name: 'Platform-Based Development',
-        code : 'IF3210',
-        description : 'lorem ipsum',
-        list_topic : [topic3.id]
-    }).save();
-
-    var program1 = await new Program({
-        name : 'Teknik Informatika',
-        description : 'lorem ipsum dono?',
-        list_teacher : [],
-        list_course : [{
-            course_id: course1.id,
-            prerequisite: []
-        },{
-            course_id: course2.id,
-            prerequisite: [course1.id]
-        }]
-    }).save();
-
-
-    var teacher1 = await new Teacher({
-        username : 'teacher1',
-        email : 'teacher1@example.com',
-        role : 1,
-        password : 'teacher1',
-        programs : [{program_id: program1.id, course_id : [course1.id, course2.id]}]
-    }).save((err, saved_test) => {
-        if (err) {console.log(err); return;}
-        Program.update(
-            { _id: program1.id }, 
-            { $push: {list_teacher: saved_test.id} }
-        );
+async function initialize() {
+  await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+    .catch((error) => {
+      throw new Error(error);
     });
 
-    var teacher2 = await new Teacher({
-        username : 'teacher2',
-        email : 'teacher2@example.com',
-        role : 1,
-        password : 'teacher2',
-        programs : [{program_id: program1.id, course_id : [course1.id]}]
-    }).save((err, saved_test) => {
-        if (err) {console.log(err); return;}
-        Program.update(
-            { _id: program1.id }, 
-            { $push: {list_teacher: saved_test.id} }
-        );
-    });
+  const topic1 = await new Topic({
+    name: 'Inheritance',
+  }).save();
 
-    var admin = await new Admin({
-        username: 'admin',
-        email: 'admin@example.com',
-        role: 2,
-        password: 'admin',
-        program_id: [program1.id]
-    }).save();
+  const topic2 = await new Topic({
+    name: 'Exception',
+  }).save();
 
-    var student1 = await new Student({
-        username: 'student1',
-        email: 'student1@example.com',
-        role: 0,
-        password: 'student1',
-        enrollprogram_id: []
-    }).save();
+  const topic3 = await new Topic({
+    name: 'Android',
+  }).save();
 
-    var enrollprogram1 = await new EnrollProgram({
-        user_id: student1.id,
-        program_id: program1.id,
-        status_program: 0,
-        courses: [{
-            course_id: course1.id,
-            status_course: 1,
-            topics: [{
-                topic_id: topic1.id,
-                status_topic: 1
-            }]
-        }]
-    }).save((err, saved_test) => {
-        if (err) {console.log(err);return;}
-        Student.update(
-            { _id: student1.id }, 
-            { $push: {enrollprogram_id: saved_test.id} }
-        );
-    });
-    console.log("done init db");
-    process.exit();
+  const course1 = await new Course({
+    name: 'Object-oriented Programming',
+    code: 'IF2110',
+    description: 'lorem ipsum',
+    list_topic: [topic1.id, topic2.id],
+  }).save();
+
+  const course2 = await new Course({
+    name: 'Platform-Based Development',
+    code: 'IF3210',
+    description: 'lorem ipsum',
+    list_topic: [topic3.id],
+  }).save();
+
+  const program1 = await new Program({
+    name: 'Teknik Informatika',
+    description: 'lorem ipsum dono?',
+    list_teacher: [],
+    list_course: [{
+      course_id: course1.id,
+      prerequisite: [],
+    }, {
+      course_id: course2.id,
+      prerequisite: [course1.id],
+    }],
+  }).save();
+
+
+  await new Teacher({
+    username: 'teacher1',
+    email: 'teacher1@example.com',
+    role: 1,
+    password: 'teacher1',
+    programs: [{ program_id: program1.id, course_id: [course1.id, course2.id] }],
+  }).save((err, savedTest) => {
+    if (err) { throw new Error(err); }
+    Program.update(
+      { _id: program1.id },
+      { $push: { list_teacher: savedTest.id } },
+    );
+  });
+
+  await new Teacher({
+    username: 'teacher2',
+    email: 'teacher2@example.com',
+    role: 1,
+    password: 'teacher2',
+    programs: [{ program_id: program1.id, course_id: [course1.id] }],
+  }).save((err, savedTest) => {
+    if (err) { throw new Error(err); }
+    Program.update(
+      { _id: program1.id },
+      { $push: { list_teacher: savedTest.id } },
+    );
+  });
+
+  await new Admin({
+    username: 'admin',
+    email: 'admin@example.com',
+    role: 2,
+    password: 'admin',
+    program_id: [program1.id],
+  }).save();
+
+  const student1 = await new Student({
+    username: 'student1',
+    email: 'student1@example.com',
+    role: 0,
+    password: 'student1',
+    enrollprogram_id: [],
+  }).save();
+
+  await new EnrollProgram({
+    user_id: student1.id,
+    program_id: program1.id,
+    status_program: 0,
+    courses: [{
+      course_id: course1.id,
+      status_course: 1,
+      topics: [{
+        topic_id: topic1.id,
+        status_topic: 1,
+      }],
+    }],
+  }).save((err, savedTest) => {
+    if (err) { throw new Error(err); }
+    Student.update(
+      { _id: student1.id },
+      { $push: { enrollprogram_id: savedTest.id } },
+    );
+  });
+  process.exit();
 }
 
 initialize();
---
+++ routes/topics.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const router = express.Router();
+
+const db = require('../db/models');
+
+
+router.get('/', (req, res) => {
+  db.Topic.find().lean().exec()
+    .then((val) => {
+      res.json({ data: val, success: !!val });
+    }, (err) => {
+      res.json({ success: false, error: err });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  db.Topic.findById(req.params.id).lean().exec()
+    .then((val) => {
+      res.json({ data: val, success: !!val });
+    }, (err) => {
+      res.json({ success: false, error: err });
+    });
+});
+
+router.post('/new', (req, res) => {
+  const { name } = req.body;
+
+  new db.Topic({ name }).save((err, saved) => {
+    if (err) { res.json({ success: false, error: err }); return; }
+
+    res.json({ success: true, id: saved.id });
+  });
+});
+
+router.patch('/edit/:id', (req, res) => {
+  const { name } = req.body;
+  db.DocsIndex.updateOne({ id: req.params.id }, { name }, (err) => {
+    if (err) { res.json({ success: false, error: err }); return; }
+
+    res.json({ success: true });
+  });
+});
+
+router.delete('/delete/:id', (req, res) => {
+  db.Topic.deleteOne({ id: req.params.id }).exec().then(
+    () => {
+      res.json({ success: true });
+    },
+  ).catch((err) => res.json({ success: false, error: err }));
+});
+
+module.exports = router;
