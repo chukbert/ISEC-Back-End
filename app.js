644c0f6530b85bc9ab25a473aa1bd4af30e76e3a
Kevin Nathaniel <kevinnathaniel497@yahoo.com>
2020-03-16 23:21:57 +0700

    Add cors handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -9,6 +10,7 @@ const topicsRouter = require('./routes/topics');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
