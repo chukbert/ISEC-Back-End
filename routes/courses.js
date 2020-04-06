eb4fe3db09f1842c02cc26165c848add685a3097
faizmuh <13517093@std.stei.itb.ac.id>
2020-04-06 17:30:54 +0700

    fix add course and add topic

--- routes/courses.js
+++ routes/courses.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const db = require('../db/models');
 
+const auth = require('../middleware/auth');
 
 router.get('/', (req, res) => {
   db.Course.find().lean().exec()
@@ -49,6 +50,62 @@ router.delete('/delete/:id', (req, res) => {
       res.json({ success: true });
     },
   ).catch((err) => res.json({ success: false, error: err }));
+});
+
+router.post('/topic/:id', auth, (req, res) => {
+  if (req.id == null) {
+    res.json({ success: false, error: 'Need Authentication Header' });
+  } else {
+    db.Admin.findById(req.id, (errAdmin, resultAdmin) => {
+      if (errAdmin) {
+        res.json({ success: false, error: errAdmin });
+      } else if (!resultAdmin) {
+        db.Teacher.findById(req.id, (errTeacher, resultTeacher) => {
+          if (errTeacher) {
+            res.json({ success: false, error: errTeacher });
+          } else if (!resultTeacher) {
+            res.json({ success: false, error: 'Admin/Teacher not found' });
+          } else {
+            // add course
+            // eslint-disable-next-line object-curly-newline
+            // eslint-disable-next-line max-len
+            // eslint-disable-next-line object-curly-newline
+            new db.Topic(req.body).save((errTopic, savedTopic) => {
+              if (errTopic) { res.json({ success: false, error: errTopic }); }
+              db.Course.findByIdAndUpdate(
+                req.params.id,
+                { $push: { list_topic: savedTopic.id } },
+                {
+                  useFindAndModify: false,
+                  new: true,
+                },
+                (errCourse) => {
+                  if (errCourse) { res.json({ success: false, error: errCourse }); }
+                },
+              );
+            });
+          }
+        });
+      } else {
+        // add course
+        // eslint-disable-next-line object-curly-newline
+        new db.Topic(req.body).save((errTopic, savedTopic) => {
+          if (errTopic) { res.json({ success: false, error: errTopic }); }
+          db.Course.findByIdAndUpdate(
+            req.params.id,
+            { $push: { list_topic: savedTopic.id } },
+            {
+              useFindAndModify: false,
+              new: true,
+            },
+            (errCourse) => {
+              if (errCourse) { res.json({ success: false, error: errCourse }); }
+            },
+          );
+        });
+      }
+    });
+  }
 });
 
 module.exports = router;
